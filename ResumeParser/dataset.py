fff8bb86c0592cd7bb03eac7ed8c587ac9a44045
Srilatha Aluru <srilatha.punna@gmail.com>
2020-11-28 17:21:15 -0600

    Delete dataset.py

--- ResumeParser/dataset.py
+++
@@ -1,8 +0,0 @@
-import utils
-
-def read_data():
-    train=utils.convert_json_to_spacy("D:\\ProjectProd\\Resume_Parser\\input\\train_1.json")
-
-    test=utils.convert_json_to_spacy("D:\\ProjectProd\\Resume_Parser\\input\\test.json")
-
-    return train,test
